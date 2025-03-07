126376a7b04950554c0792b6d547988b683e91f1
Kauã Santos Silva <106415768+kauass@users.noreply.github.com>
2025-03-06 23:08:16 -0300

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+ <h1>ka</h1>
                     {{ __('You are logged in!') }}
                 </div>
             </div>
